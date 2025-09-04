49e701a7c20d0fde34e550bf558471a2cff9dc4e
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-04 23:21:23 +0500

    feat : add result and error classes for base return type

---
+++ src/Shared/Utilities.Shared/Results/Error.cs
@@ -0,0 +1,16 @@
+﻿namespace Utilities.Shared.Results;
+
+public class Error
+{
+    public string Code { get; }
+    public string Message { get; }
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public static Error NotFound(string entity) => new Error("NotFound", $"{entity} not found.");
+    public static Error Validation(string message) => new Error("Validation", message);
+}
