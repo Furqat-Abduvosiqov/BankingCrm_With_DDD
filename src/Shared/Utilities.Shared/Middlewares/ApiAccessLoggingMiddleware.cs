74a3964b1558d6d07ad2878505859043482556a8
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-06 16:08:29 +0500

    feat: add api access logging middleware and extension

---
+++ src/Shared/Utilities.Shared/Middlewares/ApiAccessLoggingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace Utilities.Shared.Middlewares;
+
+public class ApiAccessLoggingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        context.Request.EnableBuffering();
+
+        var requestBody = "";
+        if (context.Request.ContentLength > 0 && context.Request.Body.CanSeek)
+        {
+            context.Request.Body.Position = 0;
+            using var reader = new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true);
+            requestBody = await reader.ReadToEndAsync();
+            context.Request.Body.Position = 0;
+        }
+
+        Log.Information("Handling request {Method} {Path} with body {Body}",
+            context.Request.Method,
+            context.Request.Path,
+            requestBody);
+
+        var originalBodyStream = context.Response.Body;
+        using var responseBody = new MemoryStream();
+        context.Response.Body = responseBody;
+
+        await next(context);
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+        var responseText = await new StreamReader(context.Response.Body).ReadToEndAsync();
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        Log.Information("Response {StatusCode}: {Body}",
+            context.Response.StatusCode,
+            responseText);
+
+        await responseBody.CopyToAsync(originalBodyStream);
+    }
+}
---
+++ src/Shared/Utilities.Shared/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Utilities.Shared.Middlewares;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseApiAccessLogging(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ApiAccessLoggingMiddleware>();
+    }
+}
