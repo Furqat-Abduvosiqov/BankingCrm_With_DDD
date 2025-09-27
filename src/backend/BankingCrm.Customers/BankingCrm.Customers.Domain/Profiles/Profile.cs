ca95d219d2ba7fb0db1b43148709d9752862c6c1
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-09-27 17:36:46 +0500

    feat : add two frontend project templates

---
+++ src/backend/BankingCrm.Customers/BankingCrm.Customers.Domain/Profiles/Profile.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Shared.Entities;
+
+namespace BankingCrm.Customers.Domain.Profiles;
+
+public class Profile : Entity
+{
+    public Username Username { get; set; }
+    
+}
---
+++ src/backend/BankingCrm.Customers/BankingCrm.Customers.Domain/Profiles/Username.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Shared.ValueObjects;
+
+namespace BankingCrm.Customers.Domain.Profiles;
+
+public class Username : ValueObject
+{
+    public string Value { get; set; }
+
+    public Username(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(value));
+        
+        Value = value;
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
